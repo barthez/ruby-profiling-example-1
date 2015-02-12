72aec617b3796c8cdd198e9ec3ca7baa196daf27
Bartłomiej Bułat <bartek.bulat@gmail.com>
2015-02-12 23:35:20 +0100

    Slugify employer id

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -3,11 +3,11 @@ class Employer
   include Mongoid::Timestamps
 
   field :name, type: String
-  field :_id, type: String, default: -> { name.to_param }
+  field :_id, type: String, default: -> { name.parameterize }
   field :catch_phrase, type: String
   field :logo_url, type: String
 
-  field :job_counts, type: Integer, default: 0
+  field :jobs_count, type: Integer, default: 0
 
   has_many :jobs
 end
